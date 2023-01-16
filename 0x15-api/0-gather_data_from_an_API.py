b159693c15419a15178ee6104a1dce0a37ed7f3b
MwangiGregory <gmwaburi@gmail.com>
2023-01-16 13:15:47 +0300

    Update script 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
 """This script accepts an employee ID and returns information
 about his/her TODO list progress"""
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-employee_id = sys.argv[1]
-payload = {'userId': employee_id}
-users_response = requests.get('https://jsonplaceholder.typicode.com/users',
-                              params={'id': employee_id})
-todos_response = requests.get('https://jsonplaceholder.typicode.com/todos',
-                              params=payload)
-todos = todos_response.json()
-user = users_response.json()
-todos_total = len(todos)
-employee_name = user[0].get('name')
+    employee_id = sys.argv[1]
+    payload = {'userId': employee_id}
+    users_response = requests.get('https://jsonplaceholder.typicode.com/users',
+                                params={'id': employee_id})
+    todos_response = requests.get('https://jsonplaceholder.typicode.com/todos',
+                                params=payload)
+    todos = todos_response.json()
+    user = users_response.json()
+    todos_total = len(todos)
+    employee_name = user[0].get('name')
 
-count = 0
-for todo in todos:
-    if todo.get('completed'):
-        count += 1
+    count = 0
+    for todo in todos:
+        if todo.get('completed'):
+            count += 1
 
-print(f'Employee {employee_name} is done with tasks({count}/{todos_total}):')
-for todo in todos:
-    if todo.get('completed'):
-        print(f"\t {todo.get('title')}")
+    print(f'Employee {employee_name} is done with tasks({count}/{todos_total}):')
+    for todo in todos:
+        if todo.get('completed'):
+            print(f"\t {todo.get('title')}")
         
