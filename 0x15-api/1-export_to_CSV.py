c7b80c2164e3422ef1a02740734f422391cc6612
MwangiGregory <gmwaburi@gmail.com>
2023-01-16 12:08:22 +0000

    Add script 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This script accepts an employee ID and returns information
+about his/her TODO list progress"""
+if __name__ == "__main__":
+	import requests
+	import sys
+
+	employee_id = sys.argv[1]
+	payload = {'userId': employee_id}
+	users_response = requests.get('https://jsonplaceholder.typicode.com/users',
+				  params={'id': employee_id})
+	todos_response = requests.get('https://jsonplaceholder.typicode.com/todos',
+				  params=payload)
+	todos = todos_response.json()
+	user = users_response.json()
+	todos_total = len(todos)
+	employee_name = user[0].get('name')
+
+	count = 0
+	for todo in todos:
+		if todo.get('completed'):
+		    count += 1
+
+	print('Employee {} is done with tasks({}/{}):'
+	  .format(employee_name, count, todos_total))
+	for todo in todos:
+		if todo.get('completed'):
+		    print("\t {}".format(todo.get('title')))
+
+	with open('{}.csv'.format(employee_id), 'a', encoding='utf-8') as f:
+		for todo in todos:
+			f.write('\"{}\",\"{}\",\"{}\",\"{}\"\n'
+				.format(employee_id, employee_name,
+				str(todo.get('completed')),
+				todo.get('title')))
+
