c51a4748cf6dd9cefc0f93a7946ec52d184d1c63
MwangiGregory <gmwaburi@gmail.com>
2023-01-16 18:06:21 +0300

    update script

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,23 +5,21 @@ if __name__ == "__main__":
     import json
     import requests
 
+    url = 'https://jsonplaceholder.typicode.com/'
     with open('todo_all_employees.json', 'a', encoding='utf-8') as f:
-        users = requests.get('https://jsonplaceholder.typicode.com/users')
-        users = users.json()
-        all_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-        all_todos = all_todos.json()
-
-        all_tasks = {}
+        users = requests.get(url + "users").json()
+        all_todos = {}
         for user in users:
-            usr_name = user.get('username')
+            username = user.get('username')
             user_id = user.get('id')
+            tasks = requests.get(url + "todos",
+            params={'userId': user_id}).json()
             user_tasks = []
-            for todo in all_todos:
-                if todo.get('userId') == user_id:
-                    task = {}
-                    task['username'] = usr_name
-                    task['task'] = todo.get('title')
-                    task['completed'] = todo.get('completed')
-                    user_tasks.append(task)
-            all_tasks[user_id] = user_tasks
-        json.dump(all_tasks, f)
+            for task in tasks:
+                temp = {}
+                temp['username'] = username
+                temp['task'] = task.get('title')
+                temp['completed'] = task.get('completed')
+                user_tasks.append(temp)
+            all_todos[user_id] = user_tasks
+        json.dump(all_todos, f)
