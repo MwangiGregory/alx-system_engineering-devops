131658270f56e1bede3ef0c9379d4f6113d059c6
MwangiGregory <gmwaburi@gmail.com>
2023-02-01 10:28:57 +0300

    Add script 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains function number_of_subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers of a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': "My user agent 1.0"}
+    subreddit_data = requests.get(url, allow_redirects=False, headers=headers)
+    if subreddit_data.status_code == 404:
+        return 0
+    # subscribers = subreddit_data.json().get('data').get('subscribers')
+    number_of_subscribers = subreddit_data.json()['data']['subscribers']
+    return number_of_subscribers
